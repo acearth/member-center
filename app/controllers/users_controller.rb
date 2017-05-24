a3e9a645ace6413c1cc7d596186c12eb311f1829
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2017-05-24 23:09:35 +0800

    .

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,11 +30,11 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html {redirect_to @user, notice: 'User was successfully created.'}
+        format.json {render :show, status: :created, location: @user}
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @user.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -44,11 +44,11 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        format.html {redirect_to @user, notice: 'User was successfully updated.'}
+        format.json {render :show, status: :ok, location: @user}
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @user.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -58,21 +58,23 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to users_url, notice: 'User was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:user_name, :emp_id, :mobile_phone, :email, :credential, :role, :password_digest)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    got = params.require(:user).permit(:user_name, :emp_id, :mobile_phone, :email, :password)
+    # got.merge(role: :ordinary)
+  end
+
   # Confirms the correct user.
   def correct_user
     @user = Member.find(params[:id])
