863afa73e779f7d955019b3a04acd4c7184536af
An Xiaoqiang <an_x@worksap.co.jp>
2017-05-25 21:03:15 +0800

    enable cache and preparing test

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -6,4 +6,11 @@ class TicketsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get user info by post ticket to auth" do
+    post login_url, params: {user}
+
+    post auth_url, params: {ticket: :xxxx, sign: :xxxx}
+    assert_response :success
+  end
+
 end
