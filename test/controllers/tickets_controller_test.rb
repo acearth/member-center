794f55e30e0e6f4076c4d50571a82a6014c30781
An Xiaoqiang <an_x@worksap.co.jp>
2017-05-26 13:13:30 +0800

    set production db

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -7,7 +7,7 @@ class TicketsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get user info by post ticket to auth" do
-    post login_url, params: {user}
+    # post login_url, params: {user}
 
     post auth_url, params: {ticket: :xxxx, sign: :xxxx}
     assert_response :success
