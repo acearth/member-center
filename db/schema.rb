bf4e44f942643c01ba7f4b943f9b815c6f5eaf6f
Arthur Ace (XQ.AN) <arthur4it@gmail.com>
2017-06-07 01:14:12 +0800

    add email uniqueness validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170605042821) do
+ActiveRecord::Schema.define(version: 20170606171306) do
 
   create_table "feedbacks", force: :cascade do |t|
     t.string "email"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20170605042821) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
