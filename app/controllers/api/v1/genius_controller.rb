c8029613fc22d7b4a2acbe5413ffd1a8b633323b
An Xiaoqiang <an_x@worksap.co.jp>
2017-06-08 12:42:18 +0800

    normalize API

--- app/controllers/api/v1/genius_controller.rb
+++ app/controllers/api/v1/genius_controller.rb
@@ -1,15 +1,72 @@
 class Api::V1::GeniusController < ApplicationController
+  before_action :set_service_proivider
 
+  # Params
+  #   app_id
+  #   user_name
+  #   password
+  #   sign
   def login
-    render json: {status: 0, msg: :login_sample}
+    mandatory_keys = [:app_id, :user_name, :password, :sign]
+    if params.slice(*mandatory_keys).values.any?(&:nil?)
+      render json: to_response('Some params are lost') and return
+    elsif @service_provider.nil?
+      render json: to_response('ServiceProvider error') and return
+    elsif not valid_sign?(params[:app_id], params[:user_name], params[:password], @service_provider.credential, params[:sign])
+      render json: to_response('Invalid request: parameter error') and return
+    end
+
+    user = User.find_by_user_name(params[:user_name])
+    if user && user.authenticate(params[:password])
+      render json: (user.valid_role? ? to_response('success', user) : to_response('User needs to be activated'))
+    elsif user.nil?
+      render to_response "User not exist:#{params[:user_name]}"
+    else
+      render to_response 'Password error'
+    end
   end
 
+  # Params
+  #   app_id
+  #   ticket
+  #   sign
   def authenticate
-    render json: {status: 0, msg: :authentication_sample}
+    mandatory_keys = [:app_id, :ticket, :sign]
+    if params.slice(*mandatory_keys).values.any?(&:nil?)
+      render json: to_response('Some params are lost') and return
+    elsif @service_provider.nil?
+      render json: to_response('ServiceProvider error') and return
+    elsif not valid_sign?(@service_provider.app_id, params[:ticket], @service_provider.credential, params[:sign])
+      render json: to_response("Bad request: Invalid parameter(s)") and return
+    end
+
+    ticket = Ticket.recover(@service_provider, params[:ticket])
+    if ticket.blank? || ticket.expired?
+      render json: to_response("Invalid ticket")
+    else
+      render json: to_response("success", ticket.user)
+    end
   end
 
-  def bonjour
-    logger.info 'BONJOUR...' +Time.now.to_s
-    render json: {status: 0, msg: :bonjour}
+  private
+
+  def to_response(msg, user = nil)
+    seq = Time.now.to_i #TODO-improve
+    code = msg.downcase == 'success' ? 0 : 999
+    for_sign = [seq, code, msg].map(&:to_s).join('-') + '-'
+    for_sign +=[user.user_name, user.emp_id, user.email].map(&:to_s).join('-') if user
+    signature = Digest::MD5::hexdigest(for_sign)
+    res = {seq: seq,
+           status: {code: code,
+                    msg: msg}
+    }
+    res.merge!({user: {user_name: user.user_name,
+                       employee_id: user.emp_id,
+                       email: user.email}}) if user
+    res.merge ({sign: signature})
+  end
+
+  def set_service_proivider
+    @service_provider = ServiceProvider.find_by_app_id(params[:app_id])
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,44 @@ class ApplicationController < ActionController::Base
     cookies[:locale] = params[:locale] || cookies[:locale] || I18n.default_locale
     I18n.locale = cookies[:locale]
   end
+
+  def aes_encrypt(key, text)
+    cipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    cipher.encrypt
+    cipher.key = key
+    Base64.urlsafe_encode64(cipher.update(text) + cipher.final)
+  end
+
+  def aes_decrypt(key, data)
+    decipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    decipher.decrypt
+    decipher.key = key
+    encrypted = Base64.urlsafe_decode64(data)
+    "#{decipher.update(encrypted)}#{ decipher.final}"
+  end
+
+  def format_query(url, options = [])
+    return puts "Cannot process HTTPS protocol: #{url} " if url.include?('https')
+    url = 'http://' + url unless url.start_with?('http://')
+    url.include?('?') ? url : url + '?'
+  end
+
+  def md5_sign(*info)
+    Digest::MD5::hexdigest(info.join('-'))
+  end
+
+  def signature(*info)
+    puts "USE md5_sign(...) instead"
+    Digest::MD5::hexdigest(info.join('-'))
+  end
+
+  def valid_sign?(*info, got_sign)
+    return false  unless got_sign
+    expected = Digest::MD5::hexdigest(info.join('-'))
+    return true if expected == got_sign.to_s
+    puts 'Sign Validation for:  ' + info.join(' - ')
+    puts 'Expected: ' + expected
+    puts 'GOT:      ' + got_sign
+    false
+  end
 end
