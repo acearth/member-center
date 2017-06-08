2e31a4bd30b6d9ba5227a832fb80a4822442d695
An Xiaoqiang <an_x@worksap.co.jp>
2017-06-08 17:44:34 +0800

    fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,13 +10,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_user_name(params[:session][:user_name])
     if user && user.authenticate(params[:session][:password])
-      if user.invalid_role? || !user.user_name.start_with?('test')
-        flash[:warning] = 'User not activated'
-        redirect_to root_path
-      else
+      if  !user.invalid_role? || user.user_name.start_with?('test')
         log_in user
         remember user
         redirect_to login_back(user)
+      else
+        flash[:warning] = 'User not activated'
+        redirect_to root_path
       end
     else
       flash[:warning] = I18n.t('wrong_user_or_password')
