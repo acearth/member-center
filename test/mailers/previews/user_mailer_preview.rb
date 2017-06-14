45faccae8661be5fd502ce8f0fc8231328739887
An Xiaoqiang <an_x@worksap.co.jp>
2017-06-14 11:44:40 +0800

    mailer preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,14 +1,14 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/activate
+  # Preview this email at http://localhost:13000/rails/mailers/user_mailer/activate
   def activate
-    UserMailer.activate
+    UserMailer.activate(User.last, '--info--')
   end
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  # Preview this email at http://localhost:13000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    UserMailer.password_reset(User.last,'--info--')
   end
 
 end
