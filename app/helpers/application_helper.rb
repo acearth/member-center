8d26414feeb5e6cf9dffe7913ff08db801411587
An Xiaoqiang <an_x@worksap.co.jp>
2017-06-20 09:49:59 +0800

    modify app name to genius

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = 'Member Center'
+    base_title = 'Genius Center'
     if page_title.empty?
       base_title
     else
