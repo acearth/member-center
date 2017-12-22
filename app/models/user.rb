6029fbb31b33458860302b5c25ef1e6da30a18f1
An Xiaoqiang <an_x@worksap.co.jp>
2017-12-22 18:38:44 +0800

    fix email pattern vulnerbility

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_save {email.downcase!}
   # validates :mobile_phone, phone: true #SHUT DOWN temporarily
   validates :user_name, format: {with: /\A([a-z0-9]+\.?)*[a-z0-9]+\z/, message: I18n.t('user_name_hint')}, length: {minimum: 3}, on: [:create, :save, :update]
-  validates :email, format: {with: /(\w+@worksap\.co\.jp)|(\w+@mt2015\.com)/, message: 'No Worksap Email'}, uniqueness: true, on: :create
+  validates :email, format: {with: Rails.configuration.mail[:address_regex]}, message: 'No Worksap Email'}, uniqueness : true, on : :create
   validates :password, length: {minimum: 6}, if: :password
   enum role: {ordinary: 0, admin: 1, inactive: 2, at_risk: 3, resigned: 4}
   attr_accessor :remember_token
