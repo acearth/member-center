cb5ec99b43372fc357a9ac675cb4631e8c25de78
an_x <an_x@worksap.co.jp>
2017-12-22 14:38:20 +0800

    set mail config

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,8 @@ module GeniusCenter
     config.action_mailer.default_url_options = { :host => "genius.internal.worksap.com" }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-        address: ENV['SMTP_HOST']
+        address: ENV['SMTP_HOST'],
+	enable_starttls_auto: true
     }
 
     config.active_job.queue_adapter = :sidekiq
