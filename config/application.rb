03d7df3c660eebbc2df21534f5f0e8e36e4d7df4
An Xiaoqiang <an_x@worksap.co.jp>
2018-06-26 12:33:32 +0800

    update rails 5.2 configuration

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module GeniusCenter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -25,9 +25,10 @@ module GeniusCenter
     }
 
     config.active_job.queue_adapter = :sidekiq
-    # 在使用 Ajax 处理的表单中添加真伪令牌
+    # 在使用 Ajax 处理的表单中添加真伪令牌 TODO-remove-test
     config.action_view.embed_authenticity_token_in_remote_forms = true
 
+    # TODO-change: host
     config.cache_store = :redis_store, "redis://localhost:6379/0/cache", {expires_in: 90.minutes}
 
     config.ldap = config_for(:ldap)
