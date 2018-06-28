0cac24e0a50daeb407831bd7d0a6e2e2434399d5
An Xiaoqiang <an_x@worksap.co.jp>
2018-06-28 15:24:54 +0800

    use host network and specific port

--- config/initializers/sidekiq.rb
+++
@@ -1,8 +0,0 @@
-# TODO-improve: failed to set in different ENV
-Sidekiq.configure_server do |config|
-  config.redis = {url: 'redis://redis:6379/10'}
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = {url: 'redis://redis:6379/10'}
-end
