2b1a99f6df6cad79a2b49f4dee5095742b0a929e
An Xiaoqiang <an_x@worksap.co.jp>
2018-07-19 17:58:09 +0800

    add trusted HTTP proxies to enable logging right IP address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_dispatch.trusted_proxies= ENV['TRUSTED_HTTP_PROXIES'].split(',').split.flatten
 end
