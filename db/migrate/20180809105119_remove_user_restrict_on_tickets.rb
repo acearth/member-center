9474692ae1cbd0e5e4b9149f02436bcf391c4f40
An Xiaoqiang <an_x@worksap.co.jp>
2018-08-09 19:41:41 +0800

    .

--- db/migrate/20180809105119_remove_user_restrict_on_tickets.rb
+++ db/migrate/20180809105119_remove_user_restrict_on_tickets.rb
@@ -1,6 +1,6 @@
 class RemoveUserRestrictOnTickets < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :tickets, :user_id, foreign_key: true
+    remove_reference :tickets, :user, foreign_key: true
     add_column :tickets, :user_id, :integer
   end
 end
