94dca389bf92eaee6f85cff8d6f8df27b1413a1b
An Xiaoqiang <an_x@worksap.co.jp>
2018-08-23 15:13:29 +0800

    .

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -17,7 +17,10 @@ class Admin::UsersController < ApplicationController
       end
       @users = ldap_search_users + ldap_role_users
     end
-    searched_result = User.search(params[:keywords])
+    sanitized_keywords = params[:keywords]
+    sanitized_keywords = sanitized_keywords[0..-2] if sanitized_keywords[-1] == '*'
+    sanitized_keywords = sanitized_keywords[1..-1] if sanitized_keywords[0] == '*'
+    searched_result = User.search(sanitized_keywords)
     @users += searched_result if searched_result
     @users.uniq! {|u| u.user_name}
     render 'users/index'
