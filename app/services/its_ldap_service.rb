fe3fcdae4a9cb6ee021e044f894b044c9ec8c5de
An Xiaoqiang <an_x@worksap.co.jp>
2018-12-26 17:33:33 +0800

    .

--- app/services/its_ldap_service.rb
+++ app/services/its_ldap_service.rb
@@ -7,27 +7,25 @@ require 'active_support'
 # Using User.where('email like ?', "%@ivtl%") to see IVTL users
 #
 class ItsLdapService
-  FAILED_OLD_DN = 'ou=ldap_users,dc=internal,dc=worksap,dc=com'
   HOST = ENV['ITS_LDAP_HOST'] || 'ldap-jp01.workslan'
   PORT = ENV['ITS_LDAP_PORT'] || 389
-  USER_DN = ENV['ITS_USER_DN'] || "ou=worksap.co.jp,ou=ldap_users,dc=internal,dc=worksap,dc=com"
+  USER_DN = ENV['ITS_USER_DN'] || "ou=ldap_users,dc=internal,dc=worksap,dc=com"
 
   class << self
 
     def authenticate(email, password)
-      email_prefix = email.split('@').first
       Net::LDAP.new(host: HOST, port: PORT).open do |server|
-        server.auth("uid=#{email_prefix},#{USER_DN}", password)
+        server.auth(dn_of(email), password)
         server.bind && true || false
       end
     end
 
+    # @deprecated filter
+    # filter = Net::LDAP::Filter.eq("uid", email_prefix)
+    # result = server.search(base: USER_DN, filter: filter)
     def ldap_entry(email)
-      email_prefix = email.split('@').first
       Net::LDAP.new(host: HOST, port: PORT).open do |server|
-        filter = Net::LDAP::Filter.eq("uid", email_prefix)
-        result = server.search(base: USER_DN, filter: filter)
-        return result.first if server.get_operation_result['code'] == 0
+        return server.search(base: dn_of(email)).first if server.get_operation_result['code'] == 0
         Rails.logger.error {"Failed to find ITS LDAP USER: #{email}, #{server.get_operation_result['code']}, #{server.get_operation_result['message']}"}
       end
     end
@@ -40,6 +38,11 @@ class ItsLdapService
       emp_id = ldap_entry[:employeenumber].first || 'not_given_by_ITS_authority'
       role = LdapService.role(email)
       User.new(user_name: user_name, emp_id: emp_id, email: email, display_name: display_name, role: role)
+    end
+
+    def dn_of(email)
+      prefix, suffix = email.split("@")
+      "uid=#{prefix},ou=#{suffix},#{USER_DN}"
     end
   end
 end
